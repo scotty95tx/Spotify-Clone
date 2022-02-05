5f1c7d90fc12f8867da54014d12f33f8b8798622
scotty95tx <sclowe1995@gmail.com>
2022-02-04 22:55:46 -0600

    Started to build out center page, implemented Recoil, built out Sidebar component more

---
+++ atoms/playlistAtom.js
@@ -0,0 +1,11 @@
+import { atom } from "recoil";
+
+export const playlistState = atom({
+  key: "playlistState",
+  default: null,
+});
+
+export const playlistIdState = atom({
+  key: "playlistIdState",
+  default: "3wAOAD4GwW8P26DBCkZ49o",
+});
